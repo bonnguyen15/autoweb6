f0e02859af28472bb6cd4041f94d423abc65b247
trongtin <trongtin.nguyen@diginet.com.vn>
2023-12-15 14:15:06 +0700

    v3

--- web6.py
+++ web6.py
@@ -7,9 +7,9 @@ import numpy as np
 from colorama import Fore, Back, Style
 arr = np.array([['CDN', 'HR-WEB', 'HR-API', "BI-WEB", "BI-API", "ERP-WEB", "ERP-API", "CRM-WEB", "CRM-API", "EOFFICE-WEB", "EOFFICE-API"], ['','','','','','','','','','','']], dtype=object)
 
-def Sl_Product():
+def Sl_Product(sp):
     sl = ""
-    sl = input("Vui long nhap so luong muon cai(Mac dinh la 1 neu khong nhap): ")
+    sl = input(f"Vui long nhap so luong muon cai cho {sp} (Mac dinh la 1): ")
     if sl == "" or sl <= '0':
         sl = 1
     return sl
@@ -22,17 +22,17 @@ def choices():
         choices = input('\nVui long nhap san pham can cai (Nhap 0 đe ket thuc): ')
         match choices:
             case "0": break
-            case "1": print("Ban da chon CDN"); num = int(Sl_Product()); arr[1][0] = num
-            case "2": print("Ban da chon HR-WEB"); num = int(Sl_Product()); arr[1][1] = num
-            case "3": print("Ban da chon HR-API"); num = int(Sl_Product()); arr[1][2] = num
-            case "4": print("Ban da chon BI-WEB"); num = int(Sl_Product()); arr[1][3] = num
-            case "5": print("Ban da chon BI-API"); num = int(Sl_Product()); arr[1][4] = num
-            case "6": print("Ban da chon ERP-WEB"); num = int(Sl_Product()); arr[1][5] = num
-            case "7": print("Ban da chon ERP-API"); num = int(Sl_Product()); arr[1][6] = num
-            case "8": print("Ban da chon CRM-WEB"); num = int(Sl_Product()); arr[1][7] = num
-            case "9": print("Ban da chon CRM-API"); num = int(Sl_Product()); arr[1][8] = num
-            case "10": print("Ban da chon EOFFICE-WEB"); num = int(Sl_Product()); arr[1][9] = num
-            case "11": print("Ban da chon EOFFICE-API"); num = int(Sl_Product()); arr[1][10] = num
+            case "1": print("Ban da chon CDN"); num = int(Sl_Product(arr[0][0])); arr[1][0] = num
+            case "2": print("Ban da chon HR-WEB"); num = int(Sl_Product(arr[0][1])); arr[1][1] = num
+            case "3": print("Ban da chon HR-API"); num = int(Sl_Product(arr[0][2])); arr[1][2] = num
+            case "4": print("Ban da chon BI-WEB"); num = int(Sl_Product(arr[0][3])); arr[1][3] = num
+            case "5": print("Ban da chon BI-API"); num = int(Sl_Product(arr[0][4])); arr[1][4] = num
+            case "6": print("Ban da chon ERP-WEB"); num = int(Sl_Product(arr[0][5])); arr[1][5] = num
+            case "7": print("Ban da chon ERP-API"); num = int(Sl_Product(arr[0][6])); arr[1][6] = num
+            case "8": print("Ban da chon CRM-WEB"); num = int(Sl_Product(arr[0][7])); arr[1][7] = num
+            case "9": print("Ban da chon CRM-API"); num = int(Sl_Product(arr[0][8])); arr[1][8] = num
+            case "10": print("Ban da chon EOFFICE-WEB"); num = int(Sl_Product(arr[0][9])); arr[1][9] = num
+            case "11": print("Ban da chon EOFFICE-API"); num = int(Sl_Product(arr[0][10])); arr[1][10] = num
             case _: print("Ban chua chon san pham. Vui long nhap lai")
 
 choices()
