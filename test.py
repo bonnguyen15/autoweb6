52bf944b569d51e8aad40e83a80cc0866950d0b4
trongtin <trongtin.nguyen@diginet.com.vn>
2023-12-18 15:22:05 +0700

    v5

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import string, random
-letters_and_digits = string.ascii_letters + string.digits
-random_string_and_digits=''.join(random.choice(letters_and_digits) for i in range(32))
-print("Random String and Digits: ", random_string_and_digits)
+with open ('CDN.1','r+') as f:
+    lines = f.readlines()
+    for line in lines:
+        print(line)
--- web6.py
+++ web6.py
@@ -4,12 +4,9 @@ try:
 except:
     os.system(f"pip install numpy")
 import numpy as np
-from colorama import Fore, Back, Style
 arr = np.array([['CDN', 'HR-WEB', 'HR-API', "BI-WEB", "BI-API", "ERP-WEB", "ERP-API", "CRM-WEB", "CRM-API", "EOFFICE-WEB", "EOFFICE-API"], 
-                ['','','','','','','','','','','']
-                ], dtype=object)
-
-class choicePro:
+                ['','','','','','','','','','','']], dtype=object)
+class ChoicePro:
     def __init__(self):
         pass
     def showChoice(self):
@@ -17,7 +14,7 @@ class choicePro:
             rows, cols = np.where(arr == sp)
             if arr[1][cols] != "":
                 print("Ban da chon san pham "+sp+" voi so lan cai:",arr[1][cols[0]])
-    def Sl_Product(self, sp):
+    def slProduct(self, sp):
         while True:
             sl = ""
             sl = input(f"Vui long nhap so luong muon cai cho {sp} (Mac dinh la 1): ")
@@ -35,49 +32,66 @@ class choicePro:
             choices = input('\nVui long nhap san pham can cai (Nhap 0 đe ket thuc): ')
             match choices:
                 case "0": break
-                case "1": print("Ban da chon CDN"); num = self.Sl_Product(arr[0][0]); arr[1][0] = num
-                case "2": print("Ban da chon HR-WEB"); num = self.Sl_Product(arr[0][1]); arr[1][1] = num
-                case "3": print("Ban da chon HR-API"); num = self.Sl_Product(arr[0][2]); arr[1][2] = num
-                case "4": print("Ban da chon BI-WEB"); num = self.Sl_Product(arr[0][3]); arr[1][3] = num
-                case "5": print("Ban da chon BI-API"); num = self.Sl_Product(arr[0][4]); arr[1][4] = num
-                case "6": print("Ban da chon ERP-WEB"); num = self.Sl_Product(arr[0][5]); arr[1][5] = num
-                case "7": print("Ban da chon ERP-API"); num = self.Sl_Product(arr[0][6]); arr[1][6] = num
-                case "8": print("Ban da chon CRM-WEB"); num = self.Sl_Product(arr[0][7]); arr[1][7] = num
-                case "9": print("Ban da chon CRM-API"); num = self.Sl_Product(arr[0][8]); arr[1][8] = num
-                case "10": print("Ban da chon EOFFICE-WEB"); num = int(self.Sl_Product(arr[0][9])); arr[1][9] = num
-                case "11": print("Ban da chon EOFFICE-API"); num = int(self.Sl_Product(arr[0][10])); arr[1][10] = num
+                case "1": print("Ban da chon CDN"); num = self.slProduct(arr[0][0]); arr[1][0] = num
+                case "2": print("Ban da chon HR-WEB"); num = self.slProduct(arr[0][1]); arr[1][1] = num
+                case "3": print("Ban da chon HR-API"); num = self.slProduct(arr[0][2]); arr[1][2] = num
+                case "4": print("Ban da chon BI-WEB"); num = self.slProduct(arr[0][3]); arr[1][3] = num
+                case "5": print("Ban da chon BI-API"); num = self.slProduct(arr[0][4]); arr[1][4] = num
+                case "6": print("Ban da chon ERP-WEB"); num = self.slProduct(arr[0][5]); arr[1][5] = num
+                case "7": print("Ban da chon ERP-API"); num = self.slProduct(arr[0][6]); arr[1][6] = num
+                case "8": print("Ban da chon CRM-WEB"); num = self.slProduct(arr[0][7]); arr[1][7] = num
+                case "9": print("Ban da chon CRM-API"); num = self.slProduct(arr[0][8]); arr[1][8] = num
+                case "10": print("Ban da chon EOFFICE-WEB"); num = int(self.slProduct(arr[0][9])); arr[1][9] = num
+                case "11": print("Ban da chon EOFFICE-API"); num = int(self.slProduct(arr[0][10])); arr[1][10] = num
                 case _: print("Ban chua chon san pham. Vui long nhap lai")
         self.showChoice()
+        
 class Info():
-    def __init__(self):
-        pass
-    def randomsecret(self):
+    def __init__(self, sp):
+        info = np.array([[f"Nhap duong dan chua source cho {sp} (Khong tu tao folder con trong duong dan)", "Nhap duong dan folder chua file index.html(Khong tu tao folder con trong duong dan)", "Nhap duong dan folder uploads", "Nhap ten cho API(vd:"")", "Nhap port cho API(vd:6020)", "Nhap branch name cua san pham", "Nhap link web(vd:https://dgn.com.vn)", "Nhap link api(vd:https://dgn.com.vn/api)", "Nhap ten db mongo cho API", "Nhap companycode", "Nhap duong dan folder cdn cua sp nay(vd:/home/cdn-api)", "Nhap link API-CDN(vd:https://cdgn.com.vn/cdn)", "Nhap secret API-CDN(vd:co 37 ky tu)"],
+                     ['','','','','','','','','','','','','',]], dtype=object)
+        self.info = info
+    def randomSecret(self):
         letters_and_digits = string.ascii_letters + string.digits
         random_string_and_digits=''.join(random.choice(letters_and_digits) for i in range(32))
         return random_string_and_digits
-    def writeinfo(self, sp, cols):
-        index = arr[1][cols[0]]
-        info=np.array([[f"Nhap duong dan chua source cho {sp} (Khong tu tao folder con trong duong dan)", "Nhap duong dan folder chua file index.html(Khong tu tao folder con trong duong dan)", "Nhap duong dan folder uploads", "Nhap ten cho API(vd:"")", "Nhap port cho API(vd:6020)", "Nhap branch name cua san pham", "Nhap link web(vd:https://dgn.com.vn)", "Nhap link api(vd:https://dgn.com.vn/api)", "Nhap ten db mongo cho API", "Nhap companycode", "Nhap duong dan folder cdn cua sp nay(vd:/home/cdn-api)", "Nhap link API-CDN(vd:https://cdgn.com.vn/cdn)", "Nhap secret API-CDN(vd:co 37 ky tu)"],['','','','','','','','','','','','','',]], dtype=object)
+    
+    def writeInfo(self, sp, filename):
         print(f"Vui long nhap thong tin cho san pham {sp}")
-        for i in info[0]:
-            x, y = np.where(info == i)
-            print(y[0])
+        for i in self.info[0]:
+            x, y = np.where(self.info == i)
+            print(y)
             if not (sp == 'CDN' and (y[0] == 1 or y[0] == 5 or y[0] == 9 or y[0] == 10 or y[0] == 11 or y[0] == 12)):
                 nhap = input(f"{i}: ")
-                info[1][y[0]] = nhap
-        for i in info[0]:
-            x, y = np.where(info == i)
-            if info[1][y[0]] != "":
-                with open (sp + "." + index,"a") as f:
-                    f.write(f"{i}:{info[1][y[0]]}\n")
+                self.info[1][y[0]] = nhap
+        for i in self.info[0]:
+            if self.info[1][y[0]] != "":
+                with open (filename,"a") as f:
+                    f.write(f"{i}:{self.info[1][y[0]]}\n")
+                    f.close
 
-c = choicePro()
+    def editInfo(self, sp, filename):
+        number = 0
+        with open (filename,'r+') as f:
+            lines = f.readlines()
+            print("------------------------------------------------------------------------------")
+            print(f"Thong tin ban da nhap cho san pham {sp}: ")
+            for line in lines:
+                number += 1
+                print(str(number) + line)
+            num_edit = input("Vui lòng chọn số cần chỉnh sửa: ")
+            
+
+c = ChoicePro()
 c.choices()
-cinfo = Info()
+sp = 'CDN'
 for sp in arr[0]:
+    cinfo = Info(sp)
     rows, cols = np.where(arr == sp)
     if arr[1][cols] != "":
-        cinfo.writeinfo(sp, cols)
+        filename = sp + "." + arr[1][cols[0]]
+        cinfo.writeInfo(sp, filename)
+        # cinfo.editInfo(sp, cols, filename)
 
 
 
