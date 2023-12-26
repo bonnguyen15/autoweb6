304cb8fd252d081b443991a9dd3cc8955f6d81b2
trongtin <trongtin.nguyen@diginet.com.vn>
2023-12-26 15:13:06 +0700

    v1

--- test.py
+++ test.py
@@ -1,5 +1,32 @@
+import string, random
 import numpy as np
-t = np.array([[1,2,3,'foo'], [2,3,4,'bar']])
+# input_info = np.array([[''],['']], dtype=object)
+def findSecretCDN(cdn_file):
+        with open(cdn_file,'r') as f:
+                for line in f:
+                if 'CDN_URL_INTERNAL' in line:
+                        a = line.split('"')[3]
+                        return a
+                if 'CDN_URL_PUBLIC' in line:
+                        b = line.split('"')[3]
+                        return b
+                if 'CDN_SECRET' in line:
+                        c = line.split('"')[3]
+                        return c
+                
+file_cdn = 'D:\web\cdn-api\process.json'
+cdn_private, cdn_public, secret_cdn = findSecretCDN(file_cdn)
+    
 
-rows, cols = np.where(t == 'bar')
-print("Vị trí của 'CDN' là:", rows[0], cols[0])
+# def replaceFile(self, replace_file, search_str, old_str, new_str):
+#     with open(replace_file, 'r') as f:
+#         lines = f.readlines()
+#     new_lines = []
+#     for line in lines:
+#         if search_str in line:
+#             new_line = line.replace(old_str, new_str)
+#             new_lines.append(new_line)
+#         else:
+#             new_lines.append(line)
+#     with open(replace_file, 'w') as f:
+#         f.writelines(new_lines)
