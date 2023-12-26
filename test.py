592be80c0abcacc955a99c5d073c0ce06d224bb8
trongtin <trongtin.nguyen@diginet.com.vn>
2023-12-26 17:16:05 +0700

    v3

--- test.py
+++ test.py
@@ -1,21 +1,12 @@
 import string, random
 import numpy as np
 # input_info = np.array([[''],['']], dtype=object)
-def findSecretCDN(cdn_file):
-        with open(cdn_file,'r') as f:
-                for line in f:
-                if 'CDN_URL_INTERNAL' in line:
-                        a = line.split('"')[3]
-                        return a
-                if 'CDN_URL_PUBLIC' in line:
-                        b = line.split('"')[3]
-                        return b
-                if 'CDN_SECRET' in line:
-                        c = line.split('"')[3]
-                        return c
-                
-file_cdn = 'D:\web\cdn-api\process.json'
-cdn_private, cdn_public, secret_cdn = findSecretCDN(file_cdn)
+a = 'ERP-API'
+
+if 'API' in a:
+    print('là api')
+else:
+    print('không phải api')
     
 
 # def replaceFile(self, replace_file, search_str, old_str, new_str):
